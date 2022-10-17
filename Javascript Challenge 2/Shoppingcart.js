36ded227f2bd1d9d4d8ade9b88e77eb1d7489e16
Corné Dirks <113111923+CornDoggyyy@users.noreply.github.com>
2022-10-17 16:37:45 +0200

    Add files via upload

---
+++ Javascript Challenge 2/Shoppingcart.js
@@ -0,0 +1,28 @@
+//mah super sick shopping cart
+let shoppingcart = [
+{
+    Product: "Playstation 5",
+    Price: 499,
+    Image: "images/playstation-5.jpg"
+},
+
+{
+    Product: "Cyberpunk 2049",
+    Price: 5,
+    Image: "images/cyberpunk.jpg" 
+},
+
+{
+    Product: "Playstation Controller",
+    Price: 50,
+    image: "images/controller.jpg"    
+},
+]
+
+//Search Grabbing one of these products
+shoppingcart[0]
+
+
+//filtering the products above €10,- (THE JADOR WAY)
+//let above10 = shoppingcart.filter(item => item.Price > 10) console.log(above10)
+
